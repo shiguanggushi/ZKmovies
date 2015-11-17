e4955c6f87ac49375c7a7cc88f95e73f247f8ff6
希望 <853090337@qq.com>
2015-11-17 08:39:28 +0800

    提交一次

--- js/hezuohuoban.js
+++ js/hezuohuoban.js
@@ -1,9 +1,8 @@
 jQuery(document).ready(function($){
-	$(".cusmoter-main ul li").hover(function(){
-			$(this).find(".txt").stop().animate({height:"100px"},400);
-			$(this).find(".txt h3").stop().animate({paddingTop:"6px"},400);
-		},function(){
-			$(this).find(".txt").stop().animate({height:"30px"},400);
-			$(this).find(".txt h3").stop().animate({paddingTop:"0px"},400);
-		})
-	});
+	$('.hui').mouseover(function(e) {
+        $(this).siblings().stop().fadeTo(500,0.5);
+    });
+	$('.hui').mouseout(function(e) {
+        $(this).siblings().stop().fadeTo(500,1);
+    });
+});
